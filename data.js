695122628fce8fa834591eebabc98ee068da2dfa
Adi Eyal <adi@code4sa.org>
2016-08-05 17:46:35 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,75 @@
+var data = {
+    Botswana : {
+        body : 'The painful plight of a landless farmer',
+        image : 'img/botswana.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/10/the-painful-plight-of-a-landless-farmer.html',
+    },
+    Burkina_Faso : {
+
+        body : 'Conflit de travail à Gryphon Minerals',
+        image : 'img/burkina_faso.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/08/05/conflit-de-travail-%C3%A0-gryphon-minerals.html',
+    },
+    DRC : {
+
+        body : 'Australian Mining Companies Digging A Deadly Footprint in Africa',
+        image : 'img/drc.jpg',
+        url : 'http://www.icij.org/project/fatal-extraction/australian-mining-companies-digging-deadly-footprint-africa',
+    },
+    Ghana : {
+        body : 'Dirty Diggers: A Citi News report on mining in Upper West Region',
+        image : 'img/ghana.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/08/05/dirty-diggers-a-citi-news-report-on-mining-in-upper-west-region.html',
+    },
+    Ivory_Coast : {
+        body : 'Bonikro communities disillusioned by lack of progress and development',
+        image : 'img/ivory_coast.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/08/05/bonikro-communities-disillusioned.html',
+    },
+    Madagascar : {
+        body : 'Toliara Sands – Du développement à la mauvaise gouvernance',
+        image : 'img/madagascar.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/11/toliara-sands-du-developpement-a-la-mauvaise-gouvernance.html',
+    },
+    Malawi : {
+        body : 'Paladin’s uranium waste ‘wars,’ Malawi villagers up in arms',
+        image : 'img/malawi.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/10/paladins-uranium-waste-wars-malawi-villagers-up-in-arms.html',
+    },
+    Mali : {
+        body : 'Mine de SYAMA (Mali) : Sales temps pour une société australienne',
+        image : 'img/mali.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/10/mine-de-syama-mali-sales-temps-pour-une-societe-australienne.html',
+    },
+    Namibia : {
+        body : 'Nam lucrative ground for Aussie speculators',
+        image : 'img/namibia.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/22/nam-lucrative-ground-for-aussie-speculators.html',
+    },
+    Niger : {
+        body : 'Attribution des permis miniers: eviter le syndrome des spéculateurs',
+        image : 'img/niger.jpg',
+        url : 'niger.pdf',
+    },
+    Senegal : {
+        body : 'Zircon : non-dits d’une bataille à mort pour les beaux sables de Niafrang (Exclusif)',
+        image : 'img/senegal.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/08/17/zircon-non-dits-d-une-bataille-a-mort-pour-les-beaux-sables-de-niafrang-exclusif.html',
+    },
+    South_Africa : {
+        body : 'Mining: A question of targets',
+        image : 'img/south_africa.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/17/mining-a-question-of-targets.html',
+    },
+    Tanzania : {
+        body : 'Shareholders call it golden pride mine; to residents it’s golden shame',
+        image : 'img/tanzania.jpg',
+        url : 'http://fatalextraction.investigativecenters.org/2015/07/11/shareholders-call-it-golden-pride-mine-to-residents-it-s-golden-shame.html',
+    },
+    Zambia : {
+        body : 'Munali mine residents complain of ‘open prison’ living conditions',
+        image : 'img/zambia.jpg',
+        url : 'zambia.pdf',
+    },
+};
+
